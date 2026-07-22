f4bbc3b52e9d0893eb4d2f6ce8d208dcd7fa48e3
unknown <you@example.com>
2026-07-22 17:05:04 +0000

    Completed JavaScript prompt assignment

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let name = prompt("Enter your name:");
+let age = prompt("Enter your age:");
+let email = prompt("Enter your email:");
+
+alert("Hi " + name + ", you are " + age + " years old and we'll keep in touch via your email: " + email);
